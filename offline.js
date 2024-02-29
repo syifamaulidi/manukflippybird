ea63c4f5f3167b3b8ee3bf0c5f353ec0c1d19829
syifamaulidi <161667617+syifamaulidi@users.noreply.github.com>
2024-02-29 09:08:02 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1709172405,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundflippybird-sheet0.png",
+		"images/flippybird2removebgpreview-sheet0.png",
+		"images/flippybird2removebgpreview-sheet1.png",
+		"images/pipeflippybirdremovebgpreview-sheet0.png",
+		"images/piperemovebgpreview-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
